5862dfb8fe2558e00fc24e9d70622595d116df47
leiyang <lyaa110>
2019-12-12 11:43:05 +0800

    1.测试分支合并

--- src/main/java/com/sz/earlysummer/smsbombing/controller/loginController.java
+++ src/main/java/com/sz/earlysummer/smsbombing/controller/loginController.java
@@ -1,7 +1,6 @@
 package com.sz.earlysummer.smsbombing.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
