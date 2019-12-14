b06fd54c81f3fe5fa81e09a3bfecd78e748fc47f
leiyang <lyaa110>
2019-12-14 11:12:28 +0800

    1.自动化CI/CD测试

--- src/main/java/com/sz/earlysummer/smsbombing/controller/loginController.java
+++ src/main/java/com/sz/earlysummer/smsbombing/controller/loginController.java
@@ -13,6 +13,6 @@ public class loginController {
 
     @GetMapping(value = "register/send-code")
     public String login(Object object){
-        return "success";
+        return "Java project CI/CD test.";
     }
 }
