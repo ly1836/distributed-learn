3ae020b92a2b2513ec96812261705d13ef0994df
ly@gitlab.com <12345678>
2020-03-29 21:26:15 +0800

    1.持续集成

--- src/main/java/com/sz/earlysummer/smsbombing/controller/HealthzController.java
+++ src/main/java/com/sz/earlysummer/smsbombing/controller/HealthzController.java
@@ -1,5 +1,6 @@
 package com.sz.earlysummer.smsbombing.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@Slf4j
 public class HealthzController {
     /**
      * 健康检查 服务的响应状态
      */
     @GetMapping({"/healthz"})
     public String healthz() {
+        log.info("/healthz==>ts:{}",System.currentTimeMillis());
         return "I'm fine!";
     }
 }
