27fe49fda4aa73ecc9c1f1082c99c1e0c9b23dee
ly@gitlab.com <12345678>
2020-08-03 16:39:55 +0800

    1.分布式配置中心接入

--- src/main/java/com/sz/earlysummer/smsbombing/config/AutoConfigs.java
+++ src/main/java/com/sz/earlysummer/smsbombing/config/AutoConfigs.java
@@ -2,10 +2,12 @@ package com.sz.earlysummer.smsbombing.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 @ConfigurationProperties(prefix = "pepole")
-public class TestAutoConfigs {
+public class AutoConfigs {
 
     private String name;
 }
